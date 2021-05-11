a9697f91a5937b7f0333d18a530c8962079c669e
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-05-11 10:38:03 -0500

    Added class exercise

---
+++ cat_class.py
@@ -0,0 +1,26 @@
+# OBJECT
+class Cat:
+    # Attribute!
+    species = "mammal"
+
+    # Constructor
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def describe(self):
+        return "%s is %d years old" % (self.name, self.age)
+
+
+guster = Cat("Guster", 11)
+bandit = Cat("Bandit", 11)
+
+# print("%s is %d years old" % (guster.name, guster.age))
+# print("%s is %d years old" % (bandit.name, guster.age))
+# print("%s is a %s" % (guster.name, guster.species))
+# print("%s is %s's sister, they are both %s(s)" % (bandit.name, guster.name, guster.species))
+
+# METHODS
+print(guster.describe())
+print(bandit.describe())
+
